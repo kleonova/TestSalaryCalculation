a7b5567ad04beaa1e1c63394414321c29095d6af
kleonova <kleonova0@gmail.com>
2023-11-02 17:25:14 +0800

    add sort

--- src/main/java/test/controller/EmployeeController.java
+++ src/main/java/test/controller/EmployeeController.java
@@ -2,18 +2,30 @@ package test.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import test.dto.EmployeeDto;
 import test.dto.EmployeeDtoFactory;
 import test.dto.AbstractEmployeeDto;
 import test.model.Employee;
 import test.service.EmployeeService;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.util.Comparator.*;
 
 @RestController
 @RequestMapping("/employee")
 @CrossOrigin
 public class EmployeeController {
+    Comparator<AbstractEmployeeDto> compareBySalary = new Comparator<AbstractEmployeeDto>() {
+        @Override
+        public int compare(AbstractEmployeeDto o1, AbstractEmployeeDto o2) {
+            return Double.compare(o1.getCurrentPayment(), o2.getCurrentPayment());
+        }
+    };
+
+
     @Autowired
     private EmployeeService employeeService;
 
@@ -31,7 +43,10 @@ public class EmployeeController {
                     double sumSalarySubordinates = employeeService.getSumSalarySubordinates(employee.getId());
                     AbstractEmployeeDto employeeDto = EmployeeDtoFactory.getEmployeeDto(employee, sumSalarySubordinates);
                     employeeDto.calculateCurrentPayment();
+
                     return employeeDto;
-                }).collect(Collectors.toList());
+                })
+                .sorted(compareBySalary.reversed())
+                .collect(Collectors.toList());
     }
 }
